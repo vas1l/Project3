9c907d5c24177e0a6030acaa859520d86e334f69
David A <alvarez.d@ufl.edu>
2024-04-21 16:20:03 -0400

    web app skeleton

---
+++ webapp/src/components/MainPage.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+    <section class="header">
+        <input type="text" class="country-input">
+        <h1>IMF Data Visualization</h1>
+        <div class="flex-right"></div>
+    </section>
+    <section class="data">
+        <h1>e</h1>
+    </section>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
