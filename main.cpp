bdd2d4fd2231fdee470b30bd24bd507619853c35
vas1l <vasiljevicm@ufl.edu>
2024-04-22 18:46:49 -0400

    Added loading time into Data Structures

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <chrono>
 #include "project3.h"
 #include "CsvReader.h"
 
@@ -11,7 +12,7 @@ int main() {
     trie data2;
     hashTbl data;
 
-    string file = "C:\\Users\\dalva\\OneDrive\\Desktop\\Project3\\Project3\\data.csv";
+    string file = "data.csv";
     CsvReader reader(file, ',');
     if (!reader.Parse()) {
         cerr << "Failed to open or parse the CSV file." << endl;
@@ -23,7 +24,9 @@ int main() {
         cerr << "No data in CSV file." << endl;
         return 1;
     }
-
+ 
+    // Time loading into hash
+    auto start = chrono::high_resolution_clock::now();
     for (size_t i = 1; i < rows.size(); ++i) {
         const auto& row = rows[i];
         if (row.size() < 10) continue;
@@ -50,12 +53,52 @@ int main() {
 
         c.computeTrend();
         data.addCountry(c);
-        data2.addCountry(c);
+    //    data2.addCountry(c);
     }
+    auto end = std::chrono::high_resolution_clock::now();
+   std::chrono::duration<double> elapsed_hash = end - start;
 
+    // Time loading into trie
+    auto start1 = chrono::high_resolution_clock::now();
+    for (size_t i = 1; i < rows.size(); ++i) {
+        const auto& row = rows[i];
+        if (row.size() < 10) continue;
+
+        country c;
+        try {
+            c.name = row[1];
+            c.ISO2 = row[2];
+            c.ISO3 = row[3];
+            c.Indicator = row[4];
+            c.Unit = row[5];
+            c.Source = row[6];
+            c.CTS_CODE = row[7];
+            c.CTS_NAME = row[8];
+            c.CTS_FULL_DESCRIPTION = row[9];
+            for (size_t j = 10; j < row.size(); ++j) {
+                if (!row[j].empty() && row[j] != "NA")
+                    c.temperatureChange[1961 + (j - 10)] = stod(row[j]);
+            }
+        } catch (const std::exception& e) {
+            cerr << "Error processing data for country: " << c.ISO3 << " - " << e.what() << endl;
+            continue; // Skip to next row on error
+        }
+
+        c.computeTrend();
+       // data.addCountry(c);
+       data2.addCountry(c);
+    }
+    auto end1 = std::chrono::high_resolution_clock::now();
+   std::chrono::duration<double> elapsed_trie = end1 - start1;
+
+    cout << "Time taken to load data into hash table: " << elapsed_hash.count() << "s" << endl;
+    cout << "Time taken to load data into trie: " << elapsed_trie.count() << "s" << endl;
+
+    
     data2.printRanking();
     data.computeAndPrintRanking();
-    data2.clearFile("C:\\Users\\dalva\\OneDrive\\Desktop\\Project3\\Project3\\country_data.txt");
+
+    data2.clearFile("country_data.txt");
     //data.outputCountryInfo("AFG");
     data2.outputCountryInfo2(data2.getRoot(),"USA");
 
